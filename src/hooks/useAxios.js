8a0356c6d64bd5b821bb7738fe7ff364b5998bd2
RifatRahman7 <rifatdcian@gmail.com>
2025-07-15 03:10:50 +0600

    Added firebase token

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,12 +1,29 @@
 import axios from 'axios';
+import { getAuth } from 'firebase/auth';
 
 const axiosPublic = axios.create({
   baseURL: 'http://localhost:3000',
 });
+
+axiosPublic.interceptors.request.use(
+  async (config) => {
+    const auth = getAuth();          
+    const user = auth.currentUser;  
+
+    if (user) {
+      const token = await user.getIdToken(); 
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 const useAxiosPublic = () => {
   return axiosPublic;
 };
 
 export default useAxiosPublic;
-
